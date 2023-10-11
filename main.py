5de718f5b32552468e6960f573aea943f2835b1a
Sayan Barma <sayanbarma2004@gmail.com>
2023-10-11 09:03:35 +0530

    Added more Commands
    - Added `.gitignore` to suit .py
    - Formatted the code according to PEP8 rules
    - Added the `lock windows or put pc to sleep` command
    - Added command to open news

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import wikipedia
 import webbrowser
 import pyaudio
 import os
+import ctypes
 
 
 '''
@@ -16,13 +17,18 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
+newsURL = "https://news.google.com/search?q="
 
 # Defining a talk function
+
+
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
 # Defining a take command function through which the AI will take commands
+
+
 def take_command():
     try:
         with sr.Microphone() as source:
@@ -30,15 +36,18 @@ def take_command():
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
-        
-    except:
+
+    except Exception:
         pass
     return command
 # Defining the commands and their output
+
+
 def run_jarvis():
     command = take_command()
     print("Recognizing...Created by @Yogi")
     print(command)
+
 # A command to play music on YouTube
     if 'play' in command:
         song = command.replace('play', '')
@@ -49,11 +58,12 @@ def run_jarvis():
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%M %p')
         talk('Current time is ' + time)
+
 # A command which will open youtube on google chrome
     elif 'youtube' in command:
         webbrowser.open("https://www.youtube.com/")
-        talk("YouTube has been opened...") 
-      
+        talk("YouTube has been opened...")
+
 # A command which uses the Wikipedia module to get information about people from wikipedia
     elif 'who is' in command:
         person = command.replace('who is', '')
@@ -65,11 +75,39 @@ def run_jarvis():
     elif 'best song' in command:
         webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
         talk("There you go")
-        
+
 # A command to open reddit
     elif 'reddit' in command:
         webbrowser.open("https://www.reddit.com/")
         talk("Opening Reddit...")
+
+# A command to lock windows or put it to sleep
+    elif "windows" in command or "pc" in command or "computer" in command:
+        if "lock" in command:  # lock windows
+            talk("Locking windows in 5 seconds.")
+            time.sleep(5)
+            ctypes.windll.user32.LockWorkStation()  # call to lock windows
+
+        elif "sleep" in command:  # sleep
+            talk("Windows will go to sleep in 5 seconds.")
+            time.sleep(5)
+            # call to put windows to sleep
+            os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+
+# A command to open news on a specific topic or just general news
+    elif "news" in command or "headlines" in command:
+        if "on" in command:  # check if news on specific topic
+            command = command.split("on")[1]
+            webbrowser.open_new_tab(newsURL + command)
+            talk(f"Here are some headlines on {command}\n Happy Reading.")
+        else:  # general news
+            webbrowser.open_new_tab(newsURL)
+            command(
+                'Here are some news headlines." \
+                    "Happy reading'
+            )
+
+
 '''
 Like this, you can add up any command to open any website...
 '''
